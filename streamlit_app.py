31a17748ac9c2d40621e789717aaad5ce367bc33
Rishish <rishish2712@gmail.com>
2026-05-05 14:37:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
 
 name_on_order = st.text_input('Name on Smoothie:')
 
